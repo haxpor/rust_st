c3032616a3944a8174e5ab8d6b6acd53b5d75e70
Wasin Thonkaew <haxpor@gmail.com>
2021-12-11 21:42:41 +0100

    env_var.rs: include alternative style calling Result<T>::is_ok() and its unwrap()

--- src/env_var.rs
+++ src/env_var.rs
@@ -8,9 +8,24 @@ use std::env;
 
 fn main() {
     const KEY: &str = "ENV_SECRET_KEY";
-    let env_val = env::var(KEY);
-    match env_val {
-        Ok(val) => println!("{} = {}", KEY, val),
-        Err(e) => println!("couldn't read value from {} with error '{}'", KEY, e),
+
+    // checking style 1
+    {
+        let env_val = env::var(KEY);
+        match env_val {
+            Ok(val) => println!("{} = {}", KEY, val),
+            Err(e) => println!("couldn't read value from {} with error '{}'", KEY, e),
+        }
+    }
+
+    // checking style 2
+    {
+        if env::var(KEY).is_ok() {
+            // this is usually not reccommended to use
+            // better use `match` pattern
+            println!("{} = {}", KEY, env::var(KEY).unwrap());
+        } else {
+            println!("couldn't read value from {}", KEY);
+        }
     }
 }
